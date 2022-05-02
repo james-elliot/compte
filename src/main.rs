d3190abd990817f842ec14ba5f4daeba8cd54565
Jean-Marc Alliot <jm@alliot.org>
2022-05-02 23:05:08 +0100

    Can also find all numbers for a given distribution Can find the solution to all possible problems with n numbers

--- src/main.rs
+++ src/main.rs
@@ -6,41 +6,23 @@ fn add(a:i64,b:i64) -> Option<i64> {
 }
 
 fn sub(a:i64,b:i64) -> Option<i64> {
-    if a>b {
-	let res = a-b;
-	if res != b {return Some(res)}
-	else {return None}
-    }
-    else if a<b {
-	let res = b-a;
-	if res != a {return Some(res)}
-	else {return None}
-    }
+    let res = a-b;
+    if res != b && res!=0 {return Some(res)}
     else {return None}
 }
 
 fn mul(a:i64,b:i64) -> Option<i64> {
-    if b!=1 && a!=1 {return Some(a*b)}
+    if b!=1 {return Some(a*b)}
     else {return None}
 }
 
 fn div(a:i64,b:i64) -> Option<i64> {
-    if a > b {
-	if b != 1 && a%b == 0 {
-	    let res = a/b;
-	    if res != b {return Some(res)}
-	    else {return None}
-	}
+    if b != 1 && a%b == 0 {
+	let res = a/b;
+	if res != b {return Some(res)}
 	else {return None}
     }
-    else {
-	if a != 1 && b%a == 0 {
-	    let res = b/a;
-	    if res != a {return Some(res)}
-	    else {return None}
-	}
-	else {return None}
-    }
+    else {return None}
 }
 
 const OPSN:[&str;4] = ["+","-","*","/"];
@@ -58,12 +40,10 @@ enum Computation {
 }
 
 use crate::Computation::{End,Node,Nothing};
-use core::cmp::{min,max};
 
 fn print_tree(t:Computation) {
     match t {
 	Node(i,a,b,t2) => {
-	    let (a,b)=(max(a,b),min(a,b));
 	    println!("{}{}{}={}",a,OPSN[i],b,OPS[i](a,b).unwrap());
 	    print_tree(*t2)
 	},
@@ -71,21 +51,23 @@ fn print_tree(t:Computation) {
     }
 }
 
+use core::cmp::{min,max};
 fn test(a:&Vec<i64>, goal:i64) -> Computation {
     let mut newa = Vec::with_capacity(a.len()-1);
     for i in 0..a.len() {
 	for j in i+1..a.len() {
+	    let (v1,v2)=(max(a[i],a[j]),min(a[i],a[j]));
 	    for k in 0..OPS.len() {
-		match OPS[k](a[i],a[j]) {
+		match OPS[k](v1,v2) {
 		    Some (r) => {
-			if r == goal {return Node(k,a[i],a[j],Box::new(End))}
+			if r == goal {return Node(k,v1,v2,Box::new(End))}
 			if a.len() > 2 {
 			    newa.clear();
 			    for m in 0..a.len() {if m!=i && m!=j {newa.push(a[m])}}
 			    newa.push(r);
 			    let res = test(&newa,goal);
 			    match res {
-				Node(..) => return Node(k,a[i],a[j],Box::new(res)),
+				Node(..) => return Node(k,v1,v2,Box::new(res)),
 				Nothing => {},
 				End => panic!("Never!!!")
 			    }
@@ -99,11 +81,101 @@ fn test(a:&Vec<i64>, goal:i64) -> Computation {
     return Nothing
 }
 
+fn all(a:&Vec<i64>,tab:&mut[bool]){
+    let mut newa = Vec::with_capacity(a.len()-1);
+    for i in 0..a.len() {
+	for j in i+1..a.len() {
+	    let (v1,v2)=(max(a[i],a[j]),min(a[i],a[j]));
+	    for k in 0..OPS.len() {
+		match OPS[k](v1,v2) {
+		    Some (r) => {
+			if (r as usize) < tab.len() {tab[r as usize]=true};
+			if a.len() > 2 {
+			    newa.clear();
+			    for m in 0..a.len() {
+				if m!=i && m!=j {newa.push(a[m])}
+			    }
+			    newa.push(r);
+			    all(&newa,tab);
+			    }
+		    }
+		    None => {}
+		}
+	    }
+	}
+    }
+}
+
 use std::env;
-fn main() {
+
+#[allow(dead_code)]
+fn solve_one() {
     let args: Vec<String> = env::args().collect();
     let g = args[args.len()-1].parse().unwrap();
     let mut t = Vec::with_capacity(args.len()-2);
     for i in 1..args.len()-1 {t.push(args[i].parse().unwrap())}
     print_tree(test(&t,g));
+}
+
+fn gen_numbers() -> Vec<i64> {
+    let mut t = Vec::new();
+    for i in 1..11 {t.push(i);t.push(i)}
+    t.push(25);
+    t.push(50);
+    t.push(75);
+    t.push(100);
+    return t;
+}
+
+#[allow(dead_code)]
+fn solve_all() {
+    let args: Vec<String> = env::args().collect();
+    let mut t = Vec::with_capacity(args.len()-1);
+    for i in 1..args.len() {t.push(args[i].parse().unwrap())}
+    let mut tab = [false;1000];
+    all(&t,&mut tab);
+    let mut n = 0;
+    for i in 100..tab.len() {
+	if tab[i] {n=n+1}
+    }
+    println!("{}",n);
+}
+
+const SIZE:usize = 6;
+use std::collections::HashSet;
+fn main() {
+    let nums = gen_numbers();
+    let mut ind = [0;SIZE];
+    let mut nbset = HashSet::new();
+    for i in 0..SIZE {
+	ind[i]=i;
+    }
+    let mut cnt = 0;
+    loop {
+	let mut t = Vec::with_capacity(SIZE);
+	for i in 0..SIZE {t.push(nums[ind[i]])}
+	if !nbset.contains(&t) {
+	    nbset.insert(t.clone());
+	    let mut tab = [false;1000];
+	    all(&t,&mut tab);
+	    let mut n = 0;
+	    for i in 100..tab.len() {
+		if tab[i] {n=n+1}
+	    }
+	    if n == 900 {println!("i={:?} t={:?} n={}",ind,t,n)}
+	    cnt=cnt+1;
+	}
+	for i in (0..SIZE).rev() {
+	    ind[i]=ind[i]+1;
+	    if ind[i] < nums.len()-(SIZE-1-i) {break}
+	    ind[i]=0;
+	    if i==0 {
+		println!("cnt={}",cnt);
+		return
+	    }
+	}
+	for i in 1..SIZE {
+	    if ind[i]<=ind[i-1] {ind[i]=ind[i-1]+1}
+	}
+    }
 }
